455919fa25d6bc30bbaeb5d651bf881f8d78c4f9
Adridri <64711635+Adridri24@users.noreply.github.com>
2020-11-13 06:14:44 +0100

    Fun reformating with pre-commit hook (black, flake8...)

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-import json
 import glob
+import json
 from collections import Counter
 
 
 def density(text):
-    """ 
+    """
     Renvoie un dictionnaire associant à chaque lettre sa densité, en pourcentage
     """
-    text = tuple(filter(lambda c: 'A'<=c<='Z', map(str.upper, text)))                           
-    return {l: (occur/len(text))*100 for l, occur in Counter(text).items()} 
+    text = tuple(filter(lambda c: "A" <= c <= "Z", map(str.upper, text)))
+    return {
+        letter: (occur / len(text)) * 100 for letter, occur in Counter(text).items()
+    }
+
 
 def relative_diff(density, lang):
 
     density_diff = {}
     for char, valeur in density.items():
         if (char := char.upper()) in lang:
-            density_diff[char] = abs(valeur-lang[char]) / lang[char] # écart relatif
+            density_diff[char] = abs(valeur - lang[char]) / lang[char]  # écart relatif
 
     return sum(density_diff.values())
 
 
-with open('dict_letters_repartition.json', 'r') as file:
+with open("dict_letters_repartition.json") as file:
     density_ref = json.load(file)
 
 
@@ -28,8 +31,8 @@ for filename in glob.glob("texts/*.txt"):
     print(f"\nTest avec le fichier '{filename}'")
 
     for lang, density_lang_ref in density_ref.items():
-        with open(filename, 'r') as file:
+        with open(filename) as file:
             content = file.read()
 
-        rel_diff = relative_diff(density(content), density_lang_ref) 
-        print(f"Match {lang} : {100-rel_diff}")
+        rel_diff = relative_diff(density(content), density_lang_ref)
+        print(f"Match {lang} : {100 - rel_diff}")
